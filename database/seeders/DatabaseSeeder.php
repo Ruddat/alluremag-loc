2ffcbd4e4b8f144a70aecc1dbd15f6f77e8d4c71
Ingo Ruddat <42838916+Ruddat@users.noreply.github.com>
2023-09-12 17:22:21 +0200

    Admin-User und 10 Test-User erzeugt, Faker-Sprache Deutsch

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +19,17 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        User::factory()->create([
+            'name' => 'Super Admin',
+            'email' => 'admin@admin.lt',
+            'password' => bcrypt('Ruddat66'),
+      ]);
+      User::factory()->create([
+        'name' => 'Owner',
+        'email' => 'adolf.admin@admin.lt',
+        'password' => bcrypt('Ruddat66'),
+        ]);
+
+        User::factory(10)->create();
     }
 }
