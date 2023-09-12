1be8414e46c02d97cdee33719ac07deb8270ef27
Ingo Ruddat <42838916+Ruddat@users.noreply.github.com>
2023-09-12 13:03:14 +0200

    FrontendTemplate installiert

---
+++ resources/views/frontend/body/singleplaypost.blade.php
@@ -0,0 +1,69 @@
+    <!--====== SINGLE PLAY POST PART START ======-->
+
+    <section class="single-play-post-area mt-10">
+        <div class="container custom-container">
+            <div class="single-play-box">
+                <div class="row single-play-post-slider">
+                    <div class="col-lg-6">
+                        <div class="single-play-post-item">
+                            <img src="{{ asset('frontend/assets/images/play-post-1.jpg') }}" alt="play">
+                            <div class="single-play-post-content">
+                                <div class="post-meta">
+                                    <div class="meta-categories">
+                                        <a href="#">TECHNOLOGY</a>
+                                    </div>
+                                    <div class="meta-date">
+                                        <span>March 26, 2020</span>
+                                    </div>
+                                </div>
+                                <h3 class="title"><a href="#">Success is not a good food failure makes you humble</a></h3>
+                            </div>
+                            <div class="play-btn">
+                                <a class="video-popup" href="https://www.youtube.com/watch?v=HalMzk1FFM0"><i class="fas fa-play"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="single-play-post-item">
+                            <img src="{{ asset('frontend/assets/images/play-post-2.jpg') }}" alt="play">
+                            <div class="single-play-post-content">
+                                <div class="post-meta">
+                                    <div class="meta-categories">
+                                        <a href="#">TECHNOLOGY</a>
+                                    </div>
+                                    <div class="meta-date">
+                                        <span>March 26, 2020</span>
+                                    </div>
+                                </div>
+                                <h3 class="title"><a href="#">Success is not a good food failure makes you humble</a></h3>
+                            </div>
+                            <div class="trending-btn">
+                                <a href="#"><i class="fas fa-bolt"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="single-play-post-item">
+                            <img src="{{ asset('frontend/assets/images/play-post-2.jpg') }}" alt="play">
+                            <div class="single-play-post-content">
+                                <div class="post-meta">
+                                    <div class="meta-categories">
+                                        <a href="#">TECHNOLOGY</a>
+                                    </div>
+                                    <div class="meta-date">
+                                        <span>March 26, 2020</span>
+                                    </div>
+                                </div>
+                                <h3 class="title"><a href="#">Success is not a good food failure makes you humble</a></h3>
+                            </div>
+                            <div class="trending-btn">
+                                <a href="#"><i class="fas fa-bolt"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!--====== SINGLE PLAY POST PART ENDS ======-->
